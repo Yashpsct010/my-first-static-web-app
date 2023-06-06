159f56ded2e7273786a6cb13a58f515a503c19ad
Yash Parmar <0808io201056.ies@ipsacademy.org>
2023-06-06 21:41:19 +0530

    first-azure-deployment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+// import React from 'react';
+
+// function App() {
+//   const value = 'World';
+//   return <div>Hello {value}</div>;
+// }
+
+// export default App;
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
